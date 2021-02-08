75f46bb19dfab4aaf2b7f56b5645b568d8e20b95
0z3r07 <hsutherland07@gmail.com>
2021-02-08 15:37:22 -0500

    missing file

---
+++ pipeline.py
@@ -0,0 +1,44 @@
+import subprocess
+
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+logger = logging.getLogger(__name__)
+sports_news_sites_uids = ['nbcsports', 'espn']
+
+
+def main():
+    extract()
+    transform()
+    load()
+
+
+def extract():
+    logger.info('Starting extract')
+    for sports_news_site_uid in sports_news_sites_uids:
+        subprocess.run(['python', 'main.py', sports_news_site_uid], cwd='./extract')
+        subprocess.run(['find', '.', '-name', '{}*'.format(sports_news_site_uid),
+                        '-exec', 'mv', '{}', '../transform/{}_.csv'.format(sports_news_site_uid), ';'], cwd='./extract')
+
+
+def transform():
+    logger.info('Starting transform')
+    for sports_news_site_uid in sports_news_sites_uids:
+        dirty_data_filename = '{}_.csv'.format(sports_news_site_uid)
+        clean_data_filename = 'clean_{}'.format(dirty_data_filename)
+        subprocess.run(['python', 'main.py', dirty_data_filename], cwd='./transform')
+        subprocess.run(['rm', dirty_data_filename], cwd='./transform')
+        subprocess.run(['cp', clean_data_filename, '../load/{}.csv'.format(sports_news_site_uid)], cwd='./transform')
+
+
+def load():
+    logger.info('Starting load')
+    for sports_news_site_uid in sports_news_sites_uids:
+        clean_data_filename = '{}.csv'.format(sports_news_site_uid)
+        subprocess.run(['python', 'main.py', clean_data_filename], cwd='./load')
+        subprocess.run(['rm', clean_data_filename], cwd='./load')
+
+
+if __name__ == '__main__':
+    main()
